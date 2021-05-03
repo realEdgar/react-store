99093db1d263b00ab7097da41064a2aa87b8345c
realEdgar <ing.edgar.lopez.28@gmail.com>
2021-05-03 15:07:39 -0500

    Adding icon bascket to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,13 @@ export const Header = () => {
   return (
     <header className="Header">
       <Link to="/">
-        <img src="" alt="Logo of the Store" />
-      </Link>
-      <h1 className="Header-title">Super Store</h1>
-      <div className="Header-checkout">Checkout</div>
+        <h1 className="Header-title">Super Store</h1>
+      </Link>      
+      <div className="Header-checkout">
+        <Link to="/checkout" >
+          <i className="fas fa-shopping-basket" />
+        </Link>
+      </div>
     </header>
   );
 };
