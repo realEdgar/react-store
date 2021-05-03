24e134216fd89cd09fa7c2232d296bc3de6399fa
realEdgar <ing.edgar.lopez.28@gmail.com>
2021-05-03 11:03:07 -0500

    Building the success componenet

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -3,5 +3,15 @@ import React from 'react';
 import '../styles/components/Success.css';
 
 export const Success = () => {
-  return <h1>Success Page</h1>;
+  return (
+    <section className="Success">
+      <div className="Success-content">
+        <h2>Edgar, Thanks for buying here</h2>
+        <span>Your buy has been SUCCESS</span>
+        <div className="Success-map">
+          Google Maps
+        </div>
+      </div>
+    </section>
+  );
 };
