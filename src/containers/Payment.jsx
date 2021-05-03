6b7080e679a56ed40f54e74e479233a8813039ff
realEdgar <ing.edgar.lopez.28@gmail.com>
2021-05-03 10:31:29 -0500

    Building the payment component

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -3,5 +3,12 @@ import React from 'react';
 import '../styles/components/Payment.css';
 
 export const Payment = () => {
-  return <h1>Payment Page</h1>;
+  return (
+    <main className="Payment">
+      <div className="Payment-content">
+        <h3>Resume Carry</h3>
+        <button className="Payment-button">Pay with Paypal</button>
+      </div>
+    </main>
+  );
 };
